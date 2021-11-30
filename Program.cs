bb424b97f0ce5b98d776da7d143d4562493efe67
Nadin1010 <95162095+Nadin1010@users.noreply.github.com>
2021-11-30 15:36:33 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] myArray;
+            myArray = new int[5, 5]
+    {
+        {1,0,1,0,1},
+        {0,1,0,1,0},
+        {1,0,1,0,1},
+        {0,1,0,1,0},
+        {1,0,1,0,1},
+    };
+            int height = myArray.GetLength(0);
+            int width = myArray.GetLength(1);
+
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    Console.Write(myArray[x, y] + "\t");
+                }
+                Console.WriteLine();
+                Console.ReadKey();
+            }
+        }
+    }
+}
+
+
+
+
+        
