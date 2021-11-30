e976fc61d9bd67f59db5a21bba05d03b5776c0f2
Галина <@->
2021-11-30 10:02:05 +0300

    Добавьте файлы проекта.

---
+++ Lab5/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                {
+                    int[] arrays = new int[] {  2, 4, 5, 7, 10, 11, 12 };
+                    int arraysSum = 0;
+
+                    for (int i = 0; i < arrays.Length; i++)
+                        arraysSum += arrays[i];
+
+                    Console.WriteLine("среднее арефмитическое: {0}", arraysSum / arrays.Length);
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+}
